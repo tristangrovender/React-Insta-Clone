f91ebb1b15f654bb50bfadc91e53162ede3bfa98
Tristan Grovender <therealtristangrovender@gmail.com>
2019-02-27 19:07:41 -0600

    Day 1 & 2 mostly complete

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import dummyData from  './dummy-data.js';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: [],
+      searchData: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      dummyData: dummyData
+      
+    })
+  }
+
+  searchBarHandler = event => {
+    const posts = this.state.dummyData.filter( post => {
+      if (post.username.includes(event.target.value)) {
+        return post 
+      }
+    });
+    this.setState({
+      searchData: posts
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <SearchBar searchPosts={this.searchBarHandler}/>
+       <PostContainer postData={
+         this.state.searchData.length > 0 ?
+         this.state.searchData :
+         this.state.dummyData} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
